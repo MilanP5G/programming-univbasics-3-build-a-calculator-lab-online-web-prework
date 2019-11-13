27a0caf30ae85269ecf992e87ccee689f66bd750
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-13 20:15:27 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,43 @@
 def addition(num1, num2)
-
+puts "#{num1} + #{num2}"
+num1 + num2
 end
+
+addition(5, 4)
 
 def subtraction(num1, num2)
-
+puts "#{num1} - #{num2}"
+num1 - num2
 end
+
+subtraction(10, 5)
 
 def division(num1, num2)
-
+ puts "#{num1} / #{num2}"
+ num1 / num2
 end
+
+division(50, 2)
+
 
 def multiplication(num1, num2)
-
+ puts "#{num1} * #{num2}"
+ num1 * num2
 end
+
+multiplication(4, 30)
+
 
 def modulo(num1, num2)
-
+ puts "#{num1} .remainder #{num2}"
+ num1 .remainder num2
 end
+
+modulo(34, 5)
 
 def square_root(num)
-
+ puts "maths.sqrt #{num}"
+num maths.sqrt
 end
+
+square_root(81)
